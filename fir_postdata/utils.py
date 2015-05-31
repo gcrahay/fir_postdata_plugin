f1a33a140952731a20c53ab944e6f5068ffec551
Gaetan Crahay <gaetan@crahay.eu>
2015-05-31 21:21:28 +0200

    Refactor landing view

--- fir_postdata/utils.py
+++ fir_postdata/utils.py
@@ -1,3 +1,10 @@
+from json import loads
+
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+
+from fir_nuggets import views as nuggets_views
+
 from fir_postdata.settings import settings
 
 
@@ -7,3 +14,15 @@ def get_external_values(querydict):
 		if k not in settings.POSTDATA_KEY_BLACKLIST:
 			formatted += u"{0}: {1}\n".format(k, v)
 	return formatted
+
+def add_nugget_to_event(request, event_id, redirect_object):
+		add_return = nuggets_views.new(request, event_id)
+		try:
+			result = loads(add_return.content)
+		except:
+			result = {'status': 'error'}
+		if isinstance(result, dict) and result.get('status', 'error') == 'success':
+			messages.info(request, _('Nugget added to event'))
+		else:
+			messages.error(request, _('Cannot add nugget to event'))
+		return redirect_object
--- fir_postdata/views.py
+++ fir_postdata/views.py
@@ -1,4 +1,3 @@
-from json import loads
 from datetime import datetime
 
 from django.core.urlresolvers import resolve
@@ -10,10 +9,9 @@ from django.utils.translation import ugettext_lazy as _
 
 from incidents.models import IncidentForm
 from incidents import views as incidents_views
-from fir_nuggets import views as nuggets_views
 
 from fir_postdata.forms import LandingForm
-from fir_postdata.utils import get_external_values
+from fir_postdata.utils import get_external_values, add_nugget_to_event
 
 
 @csrf_exempt
@@ -26,46 +24,32 @@ def postdata_landing(request):
 			if not form.cleaned_data.get('new', False):
 				event = form.cleaned_data.get('event', None)
 				if event is not None:
-					add_return =  nuggets_views.new(request, event.pk)
-					try:
-						result = loads(add_return.content)
-					except:
-						result = {'status':'error'}
-					if isinstance(result, dict) and result.get('status', 'error') == 'success':
-						messages.info(request, _("Nugget added to event"))
+					if event.is_incident:
+						redirect_object = redirect('incidents:details', incident_id=event.id)
 					else:
-						messages.error(request, _("Cannot add nugget to event"))
-					return redirect('incidents:details', incident_id=event.id)
+						redirect_object = redirect('events:details', incident_id=event.id)
+					return add_nugget_to_event(request, event.pk, redirect_object)
 				else:
-					messages.error(request, _("You must select an event or create one"))
+					messages.error(request, _('You must select an event or create one'))
 					if 'raw_data' not in request.POST:
 						return redirect('dashboard:main')
 			else:
 				incident_form = IncidentForm(request.POST)
 				if incident_form.is_valid():
-					creation =  incidents_views.new_event(request)
+					creation = incidents_views.new_event(request)
 					redirected = resolve(creation.url)
 					if redirected.namespace in ['incidents', 'events'] and redirected.url_name == 'details':
 						event_id = redirected.kwargs.get('incident_id')
-						add_return =  nuggets_views.new(request, event_id)
-						try:
-							result = loads(add_return.content)
-						except:
-							result = {'status':'error'}
-						if isinstance(result, dict) and result.get('status', 'error') == 'success':
-							messages.info(request, _("Nugget added to event"))
-						else:
-							messages.error(request, _("Cannot add nugget to event"))
-						return creation
+						return add_nugget_to_event(request, event_id, creation)
 				else:
 					form.errors.update(incident_form.errors)
-		elif "raw_data" in form.errors:
+		elif 'raw_data' in form.errors:
 			formatted = get_external_values(request.POST)
-			form = LandingForm(initial={'raw_data':formatted, 'new': True,
-										'source':request.META.get('HTTP_REFERER', None)})
+			form = LandingForm(initial={'raw_data': formatted, 'new': True,
+										'source': request.META.get('HTTP_REFERER', None)})
 	else:
 		formatted = get_external_values(request.GET)
-		form = LandingForm(initial={'raw_data':formatted, 'new': True,
+		form = LandingForm(initial={'raw_data': formatted, 'new': True,
 									'date': datetime.now(), 'start_timestamp': datetime.now(),
-									'source':request.META.get('HTTP_REFERER', None)})
+									'source': request.META.get('HTTP_REFERER', None)})
 	return render(request, 'postdata/landing.html', {'form': form})
